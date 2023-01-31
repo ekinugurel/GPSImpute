d98a3ba38a9666c6480d34ff0436add788f3a376
Ekin Ugurel <ugurel@uw.edu>
2023-01-31 08:25:58 -0800

    sf_taxi

--- utils/.ipynb_checkpoints/GP-checkpoint.py
+++ utils/.ipynb_checkpoints/GP-checkpoint.py
@@ -7,6 +7,10 @@ from typing import Optional
 from gpytorch.constraints import Interval, Positive
 from gpytorch.priors import Prior
 import tqdm
+import pandas as pd
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
 
 class MTGPRegressor(gpytorch.models.ExactGP):
     def __init__(self, X, y, kernel, mean=ConstantMean(), likelihood=None):
@@ -27,13 +31,58 @@ class MTGPRegressor(gpytorch.models.ExactGP):
         self.eval()
         self.likelihood.eval()
         with torch.no_grad(), gpytorch.settings.fast_pred_var():
-            self.predictions = self.likelihood(self(X))
-            self.mean = self.predictions.mean
-            self.lower, self.upper = self.predictions.confidence_region()
-            self.sigma1_l = self.predictions.mean[:, 0] - self.predictions.stddev[:, 0]
-            self.sigma1_u = self.predictions.mean[:, 0] + self.predictions.stddev[:, 0]
-            self.sigma2_l = self.predictions.mean[:, 1] - self.predictions.stddev[:, 1]
-            self.sigma2_u = self.predictions.mean[:, 1] + self.predictions.stddev[:, 1]
+            predictions = self.likelihood(self(X))
+            mean = predictions.mean
+            self.lower, self.upper = predictions.confidence_region()
+            self.sigma1_l = predictions.mean[:, 0] - predictions.stddev[:, 0]
+            self.sigma1_u = predictions.mean[:, 0] + predictions.stddev[:, 0]
+            self.sigma2_l = predictions.mean[:, 1] - predictions.stddev[:, 1]
+            self.sigma2_u = predictions.mean[:, 1] + predictions.stddev[:, 1]
+        return predictions, mean
+    
+    def plot_preds(self, mean, date_train, date_test, y_train, y_test):
+        plot_df = pd.DataFrame({'mean_lat': mean[:,0],
+                        'mean_long': mean[:,1],
+                        'lower_lat': self.lower[:,0],
+                        'lower_long': self.lower[:,1],
+                        'upper_lat': self.upper[:,0],
+                        'upper_long': self.upper[:,1],
+                        'sigma1_l': self.sigma1_l,
+                        'sigma1_u': self.sigma1_u,
+                        'sigma2_l': self.sigma2_l,
+                        'sigma2_u': self.sigma2_u,
+                       'datetime': date_test},
+                       columns=['mean_lat', 'mean_long', 'lower_lat', 
+                                'lower_long', 'upper_lat', 'upper_long', 
+                                'sigma1_l', 'sigma1_u', 'sigma2_l', 'sigma2_u', 'datetime'])
+
+        # Initialize plots
+        matplotlib.rcParams.update(matplotlib.rcParamsDefault)
+        f, (y1_ax, y2_ax) = plt.subplots(2, 1, constrained_layout = True)
+
+        y1_ax.plot(date_train, y_train[:,0].numpy(), '.', c = 'blue', label = 'Training data')
+        y1_ax.plot(date_test, plot_df['mean_lat'], '.', c='red', label = 'Predictions')
+        y1_ax.scatter(date_test, y_test[:,0].numpy(), marker='.', c='blue')
+        y1_ax.fill_between(date_test, 0, 1, where=date_test, 
+                           color='pink', alpha=0.5, label = 'Testing period', 
+                           transform=y1_ax.get_xaxis_transform())
+        y1_ax.set_title('Latitude')
+        y1_ax.set_xticks([])
+
+        y2_ax.plot(date_train, y_train[:,1].numpy(), '.', c = 'blue', label = 'Training data')
+        y2_ax.plot(date_test, plot_df['mean_long'], '.', c='red', label = 'Predictions')
+        y2_ax.scatter(date_test, y_test[:,1].numpy(), marker='.', c='blue')
+        y2_ax.fill_between(date_test, 0, 1, where=date_test, 
+                           color='pink', alpha=0.5, label = 'Testing period', 
+                           transform=y2_ax.get_xaxis_transform())
+        y2_ax.set_title('Longitude')
+        #y2_ax.set_xticks([])
+
+        plt.legend()
+        plt.xticks(rotation = 45)
+        plt.xlabel('Date', fontsize=10)
+
+        plt.show()
 
 class WhiteNoiseKernel(kernels.Kernel):
     def __init__(self, noise=1):
