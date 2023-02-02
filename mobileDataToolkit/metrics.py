6228732c6b7df766b07d854933e3efcee879e37b
Ekin Ugurel <ugurel@uw.edu>
2023-02-02 14:42:32 -0800

    benchmarking with time series methods

--- mobileDataToolkit/metrics.py
+++ mobileDataToolkit/metrics.py
@@ -2,14 +2,7 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_squared_log_error
 from sklearn.metrics import median_absolute_error
-
-
-def r2_score(y_true, y_pred, ts):
-    mu = np.mean(ts)
-    total = np.sum((y_true - mu) ** 2)
-    residuals = np.sum((y_true - y_pred) ** 2)
-    return 1 - (residuals / total)
-
+import numpy as np
 
 def absolute_percentage_error(y_true, y_pred):
     return np.abs((y_true - y_pred) / y_pred)
@@ -36,4 +29,22 @@ def evaluate(y_true, y_pred):
         'MAPE': mean_absolute_percentage_error(y_true, y_pred),
         'MAXAPE': max_absolute_percentage_error(y_true, y_pred),
         'TAPE': total_absolute_percentage_error(y_true, y_pred)
+    }
+
+def average_eval(y_true_lat, y_true_lon, y_pred_lat, y_pred_lon):
+    eval1 = evaluate(y_true_lat, y_pred_lat)
+    eval2 = evaluate(y_true_lon, y_pred_lon)
+    
+    averaged = list()
+    for i, j in zip(eval1.values(), eval2.values()):
+        averaged.append(np.sqrt(i**2 + j**2))
+        
+    return {
+        'MAE': averaged[0],
+        'RMSE': averaged[1],
+        'MAD': averaged[2],
+        #'R2': r2_score(y_true, y_pred, ts),
+        'MAPE': averaged[3],
+        'MAXAPE': averaged[4],
+        'TAPE': averaged[5]
     }
