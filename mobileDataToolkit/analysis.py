095352a6145c0b3396983055797e68701fd3d183
Ekin Ugurel <ugurel@uw.edu>
2023-02-22 23:05:02 -0800

    updated files

--- mobileDataToolkit/analysis.py
+++ mobileDataToolkit/analysis.py
@@ -6,10 +6,11 @@ def tempOcp(data, bin_len = 5):
 
     Parameters
     ----------
+    data: pd.DataFrame
+        Trajectory data that needs to be processed
+    
     bin_len : INT, optional
-    Number of minutes in a time interval (bin). The default is 5.
-    test: INT, optional
-    Boolean signifying whether to obtain the temporal occupancy of the test set only (if it exists)
+        Number of minutes in a time interval (bin). The default is 5.
 
     Returns
     -------
@@ -34,3 +35,26 @@ def tempOcp(data, bin_len = 5):
     obs.append(1)
     temp_ocp = float(len(obs) / len(bins))
     return temp_ocp
+
+def simulate_gaps(data, user_id, target_temp_ocp, bin_len=5):
+    user_data = data[data['user_id'] == user_id]
+    current_temp_ocp = tempOcp(user_data, bin_len)
+    if current_temp_ocp <= target_temp_ocp:
+        print(f"User {user_id} already has temporal occupancy of {current_temp_ocp}.")
+        return None
+    else:
+        target_bins = int(len(user_data) * target_temp_ocp)
+        current_bins = len(np.unique(user_data['unix_start_t_min'])) - 1
+        if target_bins >= current_bins:
+            print(f"Cannot decrease temporal occupancy for user {user_id} to {target_temp_ocp}.")
+            return None
+        else:
+            gap_size = int((current_bins - target_bins) / (target_bins + 1))
+            gap_starts = np.arange(gap_size, current_bins, gap_size+1)
+            gap_ends = gap_starts + int(gap_size/2)
+            for start, end in zip(gap_starts, gap_ends):
+                start_time = user_data.iloc[start]['unix_start_t_min']
+                end_time = user_data.iloc[end]['unix_start_t_min']
+                user_data = user_data[user_data['unix_start_t_min'] < start_time]\
+                            .append(user_data[user_data['unix_start_t_min'] > end_time])
+            return user_data.reset_index(drop=True)
